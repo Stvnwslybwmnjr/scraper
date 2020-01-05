c6634365ac52a38163396666cae4458096e927f1
Steven Bowman <stevenbowmanjr@gmail.com>
2020-01-05 15:30:48 -0700

    Added the commenting section from activity 20

---
+++ models/Note.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var Schema = mongoose.Schema;
+
+var NoteSchema = new Schema({
+    title: String,
+    body: String
+});
+
+var Note = mongoose.model("Note", NoteSchema);
+
+module.exports = Note;
--- server.js
+++ server.js
@@ -91,6 +91,34 @@ app.get("/articles", function(req, res) {
 });
 
 
+app.get("/articles/:id", function(req, res) {
+  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+  db.Article.findOne({ _id: req.params.id })
+    // ..and populate all of the notes associated with it
+    .populate("note")
+    .then(function(dbArticle) {
+      // If we were able to successfully find an Article with the given id, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
 
 
 
